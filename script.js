348880d9251c0b7495c2b550454bf44a8ce846fb
Jasmin7102 <56545525+Jasmin7102@users.noreply.github.com>
2021-07-26 18:51:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function showMessage() {
+	document.getElementById("result").style.display = "block";
+};
+
+function compute() {
+	var principal = document.getElementById("principal").value;
+	if(parseInt(principal) < 1) {
+		alert("Enter a positive value of Principal amount");
+		document.getElementById("principal").focus();
+		return;
+	}
+
+
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+
+	var interest = principal * years * rate / 100;
+
+	var today = new Date();
+	var yearOfWithdrawal = parseInt(today.getFullYear())+parseInt(years);
+
+	document.getElementById("deposit").innerHTML = ""+principal;
+	document.getElementById("interest").innerHTML = ""+rate;
+	document.getElementById("amount").innerHTML = ""+interest;
+	document.getElementById("year").innerHTML = ""+yearOfWithdrawal;
+	showMessage();
+
+	return false;
+};
+
+function showRange() {
+	var rate = document.getElementById("rate").value;
+	document.getElementById("ratevalue").innerHTML = rate;
+}      
